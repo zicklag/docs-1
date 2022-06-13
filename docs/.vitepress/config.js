a37d7b9af1103cb958b27e8802519445974a28df
Eron Powell <erondpowell@gmail.com>
2022-06-13 18:15:22 -0500

    added 3 levels of dropdown menus and made headway in config.js

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,35 @@
 export default {
+    base: '/',
+    lang: 'en-US',
     title: 'Directus Docs',
-    description: 'Directus. An Instant App & API for your SQL Database.'
-  }
+    description: 'Directus. An Instant App & API for your SQL Database.',
+    
+    markdown: {
+      theme: { light: 'dracula', dark:  'slack-dark' },
+      lineNumbers: true,
+      // TODO Limit to 1 level
+      // toc:
+    },
+    lastUpdated: true,
+    themeConfig: {
+      // logo: '/logo.svg', -> Need to add the logo svg 
+      // nav: [...],
+      // sidebar: { ... }
+      nav: [
+        // https://vitepress.vuejs.org/config/theme-configs.html#nav
+        { text: 'Website', link: 'https://directus.io/' },
+        { text: 'Cloud', link: 'https://directus.cloud/' },
+        { text: 'GitHub', link: 'https://github.com/directus/directus' },
+      ],
+      sidebar: [
+        // https://vitepress.vuejs.org/guide/theme-sidebar.html
+        {
+          text: 'Configuration',
+          items: [
+            // TODO: Add 3-tiers of dropdowns
+            { text: 'Getting Started', link: '/getting-started' },
+          ]
+        }
+      ]
+    }
+}
--- docs/getting-started.md
+++ docs/getting-started.md
@@ -1,1 +1,29 @@
 # Got Started
+
+[[toc]]
+## H2
+
+Come content
+
+### H3
+
+More Content
+
+- A
+- list 
+- of stuff
+
+#### h4
+
+---
+
+1. First
+2. Second
+3. Third
+
+```python
+
+for i in range(len(27)):
+    print(i)
+
+```
