101c02e8b2e9860fad8a5c78e48eb6589c4e5090
Eron Powell <erondpowell@gmail.com>
2022-07-22 13:26:17 -0500

    moved in docs

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,52 +1,613 @@
 export default {
-  base: '/',
-  lang: 'en-US',
-  title: 'Directus Docs',
-  description: 'Directus. An Instant App & API for your SQL Database.',
+  base: "/",
+  lang: "en-US",
+  title: "Directus Docs",
+  description: "Directus. An Instant App & API for your SQL Database.",
   markdown: {
-    theme: 'material-palenight',
+    theme: "material-palenight",
     toc: {
-			level: [2],
-		},
+      level: [2],
+    },
   },
   lastUpdated: true,
   themeConfig: {
-    // TODO: add the logo svg
-    // logo: '/logo.svg'
+    // TODO: Fix logo to look good in dark mode
+    logo: "/logo.svg",
+    siteTitle: false,
     nav: [
-      // https://vitepress.vuejs.org/config/theme-configs.html#nav
-      { text: 'Website', link: 'https://directus.io/' },
-      { text: 'Cloud', link: 'https://directus.cloud/' },
-      { text: 'GitHub', link: 'https://github.com/directus/directus' },
+      { text: "Website", link: "https://directus.io/" },
+      { text: "Cloud", link: "https://directus.cloud/" },
+      { text: "GitHub", link: "https://github.com/directus/directus" },
     ],
     algolia: {
       // appId: '',
       // apiKey: '',
-      indexName: 'directus'
+      indexName: "directus",
     },
-    sidebar: [
-      // NOTE: At this time, nested menus are not supported,
-      // but they plan to support in the future....
+    sidebar: sidebar(),
+    //
+    // editLink: {
+    //   pattern: 'https://github.com/directus/directus/edit/main/docs/:link',
+    //   text: 'Edit this page on GitHub'
+    // },
+  },
+};
+
+function sidebar() {
+  return [
+    // NOTE: At this time, nested menus are not supported,
+    // but they plan to support in the future....
+    {
+      text: "DEMO PAGE",
+      items: [{ text: "DEMO", link: "/DEMO/introduction" }],
+    },
+    {
+      text: "Getting Started",
+      items: [
         {
-          text: 'Getting Started',
-          items: [
-            { text: 'Introduction', link: '/getting-started/introduction' },
-          ]
+          text: "Introduction",
+          link: "/getting-started/introduction",
+          // type: "page",
         },
         {
-          text: 'Configuration',
-          collapsible: true,
-          collapsed: true,
-          items: [
-            { text: 'Data Model', link: '/configuration/data-model' },
-            { text: 'Fields', link: '/configuration/data-model/fields' },
-            { text: 'Schema', link: '/configuration/data-model/fields/schema' },
-          ]
+          text: "Quickstart Guide",
+          link: "/getting-started/quickstart",
+          // type: "page",
+        },
+        {
+          text: "Architecture",
+          link: "/getting-started/architecture",
+          // type: "page",
+        },
+        {
+          text: "Help & Support",
+          link: "/getting-started/support",
+          // type: "page",
+        },
+        {
+          text: "Backing Directus",
+          link: "/getting-started/backing-directus",
+          // type: "page",
+        },
+        {
+          text: "Resources",
+          link: "/getting-started/resources",
+          // type: "page",
+        },
+        {
+          text: "Glossary",
+          link: "/getting-started/glossary",
+          // type: "page",
         },
       ],
-      // editLink: {
-      //   pattern: 'https://github.com/directus/directus/edit/main/docs/:path',
-      //   text: 'Edit this page on GitHub'
-      // },
-  }
+    },
+    {
+      text: "App Guide",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          link: "/app/overview",
+          text: "Overview",
+          // type: "page",
+        },
+        {
+          link: "/app/content",
+          // { type: "divider" },
+          text: "Content",
+          type: "page",
+          items: [
+            {
+              link: "/app/content/collections",
+              text: "Collection Page",
+              type: "page",
+            },
+            {
+              link: "/app/content/items",
+              text: "Item Page",
+              // type: "page",
+            },
+            {
+              link: "/app/content/data-sharing",
+              text: "Data Sharing",
+              // type: "page",
+            },
+          ],
+        },
+        {
+          link: "/app/user-directory",
+          text: "User Directory",
+          // type: "page",
+        },
+        {
+          link: "/app/file-library",
+          text: "File Library",
+          // type: "page",
+        },
+        {
+          link: "/app/insights",
+          text: "Insights",
+          // type: "page",
+        },
+        {
+          link: "/app/documentation",
+          text: "Documentation",
+          // type: "page",
+        },
+        {
+          link: "/app/settings",
+          text: "Settings",
+          // type: "page",
+        },
+        {
+          link: "/app/display-templates",
+          // { type: "divider" },
+          text: "Display Templates",
+          // type: "page",
+        },
+        {
+          link: "/app/filters",
+          text: "Filters",
+          // type: "page",
+        },
+        {
+          link: "/app/layouts",
+          text: "Layouts",
+          // type: "page",
+        },
+        {
+          link: "/app/import-export",
+          text: "Import / Export",
+          // type: "page",
+        },
+      ],
+    },
+    {
+      text: "Configuration",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        // {
+        // 	type: 'page',
+        // 	link: '/configuration/overview',
+        // 	title: 'Overview',
+        // },
+        {
+          link: "/configuration/project-settings",
+          text: "Project Settings",
+          // type: "page",
+        },
+        {
+          link: "/configuration/data-model",
+          text: "Data Model",
+          // type: "page",
+          children: [
+            {
+              link: "/configuration/relationships",
+              text: "Relationships",
+              // type: "page",
+            },
+          ],
+        },
+        {
+          link: "/configuration/users-roles-permissions",
+          text: "Users, Roles & Permissions",
+          // type: "page",
+        },
+        {
+          link: "/configuration/presets-bookmarks",
+          text: "Presets & Bookmarks",
+          // type: "page",
+        },
+        {
+          link: "/configuration/translation-strings",
+          text: "Translation Strings",
+          // type: "page",
+        },
+        {
+          link: "/configuration/webhooks",
+          text: "Webhooks",
+          // type: "page",
+        },
+        {
+          link: "/configuration/flows",
+          text: "Flows",
+          // type: "page",
+          collapsable: false,
+          children: [
+            {
+              link: "/configuration/flows/triggers",
+              text: "Triggers",
+              // type: "page",
+            },
+            {
+              link: "/configuration/flows/operations",
+              text: "Operations",
+              // type: "page",
+            },
+          ],
+        },
+        {
+          link: "/configuration/activity-log",
+          text: "Activity Log",
+          // type: "page",
+        },
+        // {
+        // 	link: '/configuration/security',
+        // 	text: 'Security*',
+        // 	type: 'page',
+        // },
+        // {
+        // 	link: '/configuration/localization',
+        // 	text: 'Localization*',
+        // 	type: 'page',
+        // },
+        // {
+        // 	link: '/configuration/faq',
+        // 	text: 'Frequently Asked Questions*',
+        // 	type: 'page',
+        // },
+      ],
+    },
+    {
+      text: "API Reference",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          // type: 'page',
+          link: "/reference/introduction",
+          text: "Introduction",
+        },
+        {
+          // type: 'page',
+          link: "/reference/authentication",
+          text: "Authentication",
+        },
+        {
+          // type: 'page',
+          link: "/reference/query",
+          text: "Global Parameters",
+        },
+        {
+          // type: 'page',
+          link: "/reference/filter-rules",
+          text: "Filter Rules",
+        },
+        {
+          // type: 'page',
+          link: "/reference/items",
+          text: "Items",
+        },
+        {
+          // type: 'page',
+          link: "/reference/files",
+          text: "Files",
+        },
+        {
+          // type: 'divider',
+        },
+        {
+          // type: 'page',
+          link: "/reference/cli",
+          text: "CLI",
+        },
+        {
+          // type: 'page',
+          link: "/reference/sdk",
+          text: "JS-SDK",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/activity",
+          text: "Activity",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/collections",
+          text: "Collections",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/extensions",
+          text: "Extensions",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/fields",
+          text: "Fields",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/flows",
+          text: "Flows",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/folders",
+          text: "Folders",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/notifications",
+          text: "Notifications",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/operations",
+          text: "Operations",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/permissions",
+          text: "Permissions",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/presets",
+          text: "Presets",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/relations",
+          text: "Relations",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/revisions",
+          text: "Revisions",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/roles",
+          text: "Roles",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/server",
+          text: "Server",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/settings",
+          text: "Settings",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/users",
+          text: "Users",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/utilities",
+          text: "Utilities",
+        },
+        {
+          // type: 'page',
+          link: "/reference/system/webhooks",
+          text: "Webhooks",
+        },
+      ],
+    },
+    {
+      text: "Extensions",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          // type: 'page',
+          link: "/extensions/introduction",
+          text: "Introduction",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/creating-extensions",
+          text: "Creating Extensions",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/displays",
+          text: "Displays",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/email-templates",
+          text: "Email Templates",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/endpoints",
+          text: "Endpoints",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/hooks",
+          text: "Hooks",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/interfaces",
+          text: "Interfaces",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/layouts",
+          text: "Layouts",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/migrations",
+          text: "Migrations",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/modules",
+          text: "Modules",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/panels",
+          text: "Panels",
+        },
+        {
+          // type: 'page',
+          link: "/extensions/themes",
+          text: "Themes",
+        },
+      ],
+    },
+    {
+      text: "Contributing",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          // type: 'page',
+          link: "/contributing/introduction",
+          text: "Introduction",
+        },
+        {
+          // type: 'page',
+          link: "/contributing/codebase-overview",
+          text: "Codebase Overview",
+        },
+        {
+          // type: 'page',
+          link: "/contributing/running-locally",
+          text: "Running Locally",
+        },
+        {
+          // type: 'page',
+          link: "/contributing/github-ci",
+          text: "GitHub CI",
+        },
+        {
+          // type: 'page',
+          link: "/contributing/translations",
+          text: "Translating the App",
+        },
+      ],
+    },
+    {
+      text: "Directus Cloud",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          // type: "page",
+          link: "/cloud/overview",
+          text: "Overview",
+        },
+        {
+          // type: "page",
+          link: "/cloud/accounts",
+          text: "Accounts",
+        },
+        {
+          // type: "page",
+          link: "/cloud/teams",
+          text: "Teams",
+        },
+        {
+          // type: "page",
+          link: "/cloud/projects",
+          text: "Projects",
+        },
+        {
+          // type: "page",
+          link: "/cloud/glossary",
+          text: "Glossary",
+        },
+      ],
+    },
+    {
+      text: "Self Hosted",
+      collapsible: true,
+      collapsed: true,
+      items: [
+        {
+          // type: "page",
+          link: "/self-hosted/quickstart",
+          text: "Quickstart",
+        },
+        {
+          // type: "page",
+          link: "/self-hosted/config-options",
+          text: "Config Options",
+        },
+        {
+          // type: "page",
+          link: "/self-hosted/sso",
+          text: "Single Sign-On (SSO)",
+        },
+        // {
+        // 	type: 'page',
+        // 	link: '/self-hosted/sso-examples',
+        // 	text: 'SSO Examples',
+        // },
+        {
+          // type: "page",
+          link: "/self-hosted/installation",
+          text: "Installation",
+          items: [
+            {
+              // type: "page",
+              link: "/self-hosted/installation/aws",
+              text: "AWS",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/cloudron",
+              text: "Cloudron",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/cli",
+              text: "CLI",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/digitalocean-app-platform",
+              text: "DigitalOcean",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/docker",
+              text: "Docker",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/gcp",
+              text: "Google Cloud Platform",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/iis",
+              text: "IIS",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/manual",
+              text: "Manual",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/plesk",
+              text: "Plesk",
+            },
+            {
+              // type: "page",
+              link: "/self-hosted/installation/ubuntu",
+              text: "Ubuntu",
+            },
+          ],
+        },
+        {
+          type: "page",
+          link: "/self-hosted/upgrades-migrations",
+          text: "Upgrades & Migrations",
+        },
+      ],
+    },
+  ];
 }
--- docs/index.md
+++ docs/index.md
@@ -1,3 +1,28 @@
 ---
 layout: home
+
+title: Directus
+titleTemplate: An Instant App & API for your SQL Database.
+
+hero:
+  name: Directus
+  text: An Instant App & API for your SQL Database.
+  tagline: Power any project with a modern, open source data platform
+  actions:
+    - theme: brand
+      text: Get Started
+      link: /getting-started/quickstart
+    - theme: alt
+      text: View on GitHub
+      link: https://github.com/directus/directus
+
+features:
+  - title: Designed for Developers
+    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+  - title: Wraps any SQL database
+    details: Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+  - title: Code-free Data Studio
+    details: Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam.
+  - title:  REST and GraphQL APIs
+    details: Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur.
 ---
