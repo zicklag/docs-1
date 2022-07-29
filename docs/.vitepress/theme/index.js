9d8b1f6ce712fec4504bd1fa1034dc4fd3e35c86
Eron Powell <erondpowell@gmail.com>
2022-07-29 09:12:59 -0500

    index.js and index.md tweaks

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,11 +1,8 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
+
 import "./vars.css";
 
 export default {
   ...DefaultTheme,
-  enhanceApp({ app }) {
-    // register global components
-    // app.component('MyGlobalComponent', /* ... */)
-  },
 };
--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,3 @@
 <!--@include: ./getting-started/introduction.md-->
+<!-- THIS IS A TEMPORARY FIX. Here's the better solution. -->
+<!-- https://discord.com/channels/325477692906536972/790509637598314527/1002298317608333472 -->
