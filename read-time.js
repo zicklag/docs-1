547f231fa169408bfbc766f6ed5cd4564014b33a
brainslug <tim@brainslug.nl>
2022-08-01 17:17:35 +0200

    hacky read time script

---
+++ read-time.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const glob = require('glob');
+const readingTime = require('reading-time');
+
+const getFiles = (src, callback) => glob(src + '/**/*.md', callback);
+getFiles('docs', function (err, res) {
+    if (err) return console.log('Error', err);
+    for (const filePath of res) {
+        try {
+            const content = fs.readFileSync(filePath, 'utf8');
+            const readTime = readingTime(content);
+            if (content.indexOf('readTime:') > 0) {
+                fs.writeFileSync(filePath, content.replace(/^readTime:\s?.+$/gm, `readTime: ${readTime.text}`));
+            }
+            // console.log(filePath, readTime);
+        } catch (err) {
+            // console.error(err);
+        }
+    }
+});
