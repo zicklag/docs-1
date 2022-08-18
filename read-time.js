8b9ee2ba7b82bed83a5031c840228cdf8ade6940
brainslug <tim@brainslug.nl>
2022-08-18 18:29:09 +0200

    changed the script to count words (sort of)

--- read-time.js
+++ read-time.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const glob = require('glob');
-const readingTime = require('reading-time');
+// const readingTime = require('reading-time');
 
 const getFiles = (src, callback) => glob(src + '/**/*.md', callback);
 getFiles('docs', function (err, res) {
@@ -8,10 +8,11 @@ getFiles('docs', function (err, res) {
     for (const filePath of res) {
         try {
             const content = fs.readFileSync(filePath, 'utf8');
-            const readTime = readingTime(content);
-            if (content.indexOf('readTime:') > 0) {
-                fs.writeFileSync(filePath, content.replace(/^readTime:\s?.+$/gm, `readTime: ${readTime.text}`));
-            }
+            // const readTime = readingTime(content);
+            console.log(`[${filePath}] ${content.split(' ').length}`);
+            // if (content.indexOf('readTime:') > 0) {
+            //     fs.writeFileSync(filePath, content.replace(/^readTime:\s?.+$/gm, `readTime: ${readTime.text}`));
+            // }
             // console.log(filePath, readTime);
         } catch (err) {
             // console.error(err);
